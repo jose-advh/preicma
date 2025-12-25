92d11a7fad2be7a024f026d6724e8b2c1389e16f
Jose Diaz <josearmandodiazhernandez1@gmail.com>
2025-12-25 15:48:11 -0500

    Errores corregidos

--- preicma-app/src/services/usuarioService.js
+++ preicma-app/src/services/usuarioService.js
@@ -1,4 +1,4 @@
-import { supabase } from "@/lib/supabaseClient";
+import { supabase } from "../lib/supabaseClient";
 
 export const crearUsuario = async (usuario) => {
   const { data, error } = await supabase.from("usuarios").insert(usuario);
